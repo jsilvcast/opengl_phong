5986bf035b41dca0b3b39f8b678bb8c2301d1b03
v <josesilvacastro1994@gmail.com>
2023-06-07 19:27:48 +0200

    Changed shader names and indentation

--- spinningcube_withlight_SKEL.cpp
+++ spinningcube_withlight_SKEL.cpp
@@ -26,8 +26,8 @@ GLuint vao = 0; // Vertext Array Object to set input data
 GLint model_location, view_location, proj_location; // Uniforms for transformation matrices
 
 // Shader names
-const char *vertexFileName = "spinningcube_withlight_vs.glsl";
-const char *fragmentFileName = "spinningcube_withlight_fs.glsl";
+const char *vertexFileName = "../spinningcube_withlight_vs_SKEL.glsl";
+const char *fragmentFileName = "../spinningcube_withlight_fs_SKEL.glsl";
 
 // Camera
 glm::vec3 camera_pos(0.0f, 0.0f, 3.0f);
@@ -45,249 +45,249 @@ glm::vec3 material_specular(0.5f, 0.5f, 0.5f);
 const GLfloat material_shininess = 32.0f;
 
 int main() {
-  // start GL context and O/S window using the GLFW helper library
-  if (!glfwInit()) {
-    fprintf(stderr, "ERROR: could not start GLFW3\n");
-    return 1;
-  }
+    // start GL context and O/S window using the GLFW helper library
+    if (!glfwInit()) {
+        fprintf(stderr, "ERROR: could not start GLFW3\n");
+        return 1;
+    }
 
-  //  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-  //  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-  //  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-  //  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    //  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    //  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    //  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-  GLFWwindow* window = glfwCreateWindow(gl_width, gl_height, "My spinning cube", NULL, NULL);
-  if (!window) {
-    fprintf(stderr, "ERROR: could not open window with GLFW3\n");
-    glfwTerminate();
-    return 1;
-  }
-  glfwSetWindowSizeCallback(window, glfw_window_size_callback);
-  glfwMakeContextCurrent(window);
+    GLFWwindow* window = glfwCreateWindow(gl_width, gl_height, "My spinning cube", NULL, NULL);
+    if (!window) {
+        fprintf(stderr, "ERROR: could not open window with GLFW3\n");
+        glfwTerminate();
+        return 1;
+    }
+    glfwSetWindowSizeCallback(window, glfw_window_size_callback);
+    glfwMakeContextCurrent(window);
 
-  // start GLEW extension handler
-  // glewExperimental = GL_TRUE;
-  glewInit();
+    // start GLEW extension handler
+    // glewExperimental = GL_TRUE;
+    glewInit();
 
-  // get version info
-  const GLubyte* vendor = glGetString(GL_VENDOR); // get vendor string
-  const GLubyte* renderer = glGetString(GL_RENDERER); // get renderer string
-  const GLubyte* glversion = glGetString(GL_VERSION); // version as a string
-  const GLubyte* glslversion = glGetString(GL_SHADING_LANGUAGE_VERSION); // version as a string
-  printf("Vendor: %s\n", vendor);
-  printf("Renderer: %s\n", renderer);
-  printf("OpenGL version supported %s\n", glversion);
-  printf("GLSL version supported %s\n", glslversion);
-  printf("Starting viewport: (width: %d, height: %d)\n", gl_width, gl_height);
+    // get version info
+    const GLubyte* vendor = glGetString(GL_VENDOR); // get vendor string
+    const GLubyte* renderer = glGetString(GL_RENDERER); // get renderer string
+    const GLubyte* glversion = glGetString(GL_VERSION); // version as a string
+    const GLubyte* glslversion = glGetString(GL_SHADING_LANGUAGE_VERSION); // version as a string
+    printf("Vendor: %s\n", vendor);
+    printf("Renderer: %s\n", renderer);
+    printf("OpenGL version supported %s\n", glversion);
+    printf("GLSL version supported %s\n", glslversion);
+    printf("Starting viewport: (width: %d, height: %d)\n", gl_width, gl_height);
 
-  // Enable Depth test: only draw onto a pixel if fragment closer to viewer
-  glEnable(GL_DEPTH_TEST);
-  glDepthFunc(GL_LESS); // set a smaller value as "closer"
+    // Enable Depth test: only draw onto a pixel if fragment closer to viewer
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LESS); // set a smaller value as "closer"
 
-  // Vertex Shader
-  char* vertex_shader = textFileRead(vertexFileName);
+    // Vertex Shader
+    char* vertex_shader = textFileRead(vertexFileName);
 
-  // Fragment Shader
-  char* fragment_shader = textFileRead(fragmentFileName);
+    // Fragment Shader
+    char* fragment_shader = textFileRead(fragmentFileName);
 
-  // Shaders compilation
-  GLuint vs = glCreateShader(GL_VERTEX_SHADER);
-  glShaderSource(vs, 1, &vertex_shader, NULL);
-  free(vertex_shader);
-  glCompileShader(vs);
+    // Shaders compilation
+    GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vs, 1, &vertex_shader, NULL);
+    free(vertex_shader);
+    glCompileShader(vs);
 
-  int  success;
-  char infoLog[512];
-  glGetShaderiv(vs, GL_COMPILE_STATUS, &success);
-  if (!success) {
-    glGetShaderInfoLog(vs, 512, NULL, infoLog);
-    printf("ERROR: Vertex Shader compilation failed!\n%s\n", infoLog);
+    int  success;
+    char infoLog[512];
+    glGetShaderiv(vs, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(vs, 512, NULL, infoLog);
+        printf("ERROR: Vertex Shader compilation failed!\n%s\n", infoLog);
 
-    return(1);
-  }
+        return(1);
+    }
 
-  GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
-  glShaderSource(fs, 1, &fragment_shader, NULL);
-  free(fragment_shader);
-  glCompileShader(fs);
+    GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fs, 1, &fragment_shader, NULL);
+    free(fragment_shader);
+    glCompileShader(fs);
 
-  glGetShaderiv(fs, GL_COMPILE_STATUS, &success);
-  if (!success) {
-    glGetShaderInfoLog(fs, 512, NULL, infoLog);
-    printf("ERROR: Fragment Shader compilation failed!\n%s\n", infoLog);
+    glGetShaderiv(fs, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(fs, 512, NULL, infoLog);
+        printf("ERROR: Fragment Shader compilation failed!\n%s\n", infoLog);
 
-    return(1);
-  }
+        return(1);
+    }
 
-  // Create program, attach shaders to it and link it
-  shader_program = glCreateProgram();
-  glAttachShader(shader_program, fs);
-  glAttachShader(shader_program, vs);
-  glLinkProgram(shader_program);
+    // Create program, attach shaders to it and link it
+    shader_program = glCreateProgram();
+    glAttachShader(shader_program, fs);
+    glAttachShader(shader_program, vs);
+    glLinkProgram(shader_program);
 
-  glValidateProgram(shader_program);
-  glGetProgramiv(shader_program, GL_LINK_STATUS, &success);
-  if(!success) {
-    glGetProgramInfoLog(shader_program, 512, NULL, infoLog);
-    printf("ERROR: Shader Program linking failed!\n%s\n", infoLog);
+    glValidateProgram(shader_program);
+    glGetProgramiv(shader_program, GL_LINK_STATUS, &success);
+    if(!success) {
+        glGetProgramInfoLog(shader_program, 512, NULL, infoLog);
+        printf("ERROR: Shader Program linking failed!\n%s\n", infoLog);
 
-    return(1);
-  }
+        return(1);
+    }
 
-  // Release shader objects
-  glDeleteShader(vs);
-  glDeleteShader(fs);
+    // Release shader objects
+    glDeleteShader(vs);
+    glDeleteShader(fs);
 
-  // Vertex Array Object
-  glGenVertexArrays(1, &vao);
-  glBindVertexArray(vao);
+    // Vertex Array Object
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
 
-  // Cube to be rendered
-  //
-  //          0        3
-  //       7        4 <-- top-right-near
-  // bottom
-  // left
-  // far ---> 1        2
-  //       6        5
-  //
-  const GLfloat vertex_positions[] = {
-    -0.25f, -0.25f, -0.25f, // 1
-    -0.25f,  0.25f, -0.25f, // 0
-     0.25f, -0.25f, -0.25f, // 2
+    // Cube to be rendered
+    //
+    //          0        3
+    //       7        4 <-- top-right-near
+    // bottom
+    // left
+    // far ---> 1        2
+    //       6        5
+    //
+    const GLfloat vertex_positions[] = {
+            -0.25f, -0.25f, -0.25f, // 1
+            -0.25f,  0.25f, -0.25f, // 0
+            0.25f, -0.25f, -0.25f, // 2
 
-     0.25f,  0.25f, -0.25f, // 3
-     0.25f, -0.25f, -0.25f, // 2
-    -0.25f,  0.25f, -0.25f, // 0
+            0.25f,  0.25f, -0.25f, // 3
+            0.25f, -0.25f, -0.25f, // 2
+            -0.25f,  0.25f, -0.25f, // 0
 
-     0.25f, -0.25f, -0.25f, // 2
-     0.25f,  0.25f, -0.25f, // 3
-     0.25f, -0.25f,  0.25f, // 5
+            0.25f, -0.25f, -0.25f, // 2
+            0.25f,  0.25f, -0.25f, // 3
+            0.25f, -0.25f,  0.25f, // 5
 
-     0.25f,  0.25f,  0.25f, // 4
-     0.25f, -0.25f,  0.25f, // 5
-     0.25f,  0.25f, -0.25f, // 3
+            0.25f,  0.25f,  0.25f, // 4
+            0.25f, -0.25f,  0.25f, // 5
+            0.25f,  0.25f, -0.25f, // 3
 
-     0.25f, -0.25f,  0.25f, // 5
-     0.25f,  0.25f,  0.25f, // 4
-    -0.25f, -0.25f,  0.25f, // 6
+            0.25f, -0.25f,  0.25f, // 5
+            0.25f,  0.25f,  0.25f, // 4
+            -0.25f, -0.25f,  0.25f, // 6
 
-    -0.25f,  0.25f,  0.25f, // 7
-    -0.25f, -0.25f,  0.25f, // 6
-     0.25f,  0.25f,  0.25f, // 4
+            -0.25f,  0.25f,  0.25f, // 7
+            -0.25f, -0.25f,  0.25f, // 6
+            0.25f,  0.25f,  0.25f, // 4
 
-    -0.25f, -0.25f,  0.25f, // 6
-    -0.25f,  0.25f,  0.25f, // 7
-    -0.25f, -0.25f, -0.25f, // 1
+            -0.25f, -0.25f,  0.25f, // 6
+            -0.25f,  0.25f,  0.25f, // 7
+            -0.25f, -0.25f, -0.25f, // 1
 
-    -0.25f,  0.25f, -0.25f, // 0
-    -0.25f, -0.25f, -0.25f, // 1
-    -0.25f,  0.25f,  0.25f, // 7
+            -0.25f,  0.25f, -0.25f, // 0
+            -0.25f, -0.25f, -0.25f, // 1
+            -0.25f,  0.25f,  0.25f, // 7
 
-     0.25f, -0.25f, -0.25f, // 2
-     0.25f, -0.25f,  0.25f, // 5
-    -0.25f, -0.25f, -0.25f, // 1
+            0.25f, -0.25f, -0.25f, // 2
+            0.25f, -0.25f,  0.25f, // 5
+            -0.25f, -0.25f, -0.25f, // 1
 
-    -0.25f, -0.25f,  0.25f, // 6
-    -0.25f, -0.25f, -0.25f, // 1
-     0.25f, -0.25f,  0.25f, // 5
+            -0.25f, -0.25f,  0.25f, // 6
+            -0.25f, -0.25f, -0.25f, // 1
+            0.25f, -0.25f,  0.25f, // 5
 
-     0.25f,  0.25f,  0.25f, // 4
-     0.25f,  0.25f, -0.25f, // 3
-    -0.25f,  0.25f,  0.25f, // 7
+            0.25f,  0.25f,  0.25f, // 4
+            0.25f,  0.25f, -0.25f, // 3
+            -0.25f,  0.25f,  0.25f, // 7
 
-    -0.25f,  0.25f, -0.25f, // 0
-    -0.25f,  0.25f,  0.25f, // 7
-     0.25f,  0.25f, -0.25f  // 3
-  };
+            -0.25f,  0.25f, -0.25f, // 0
+            -0.25f,  0.25f,  0.25f, // 7
+            0.25f,  0.25f, -0.25f  // 3
+    };
 
 // Vertex Buffer Object (for vertex coordinates)
-  GLuint vbo = 0;
-  glGenBuffers(1, &vbo);
-  glBindBuffer(GL_ARRAY_BUFFER, vbo);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(vertex_positions), vertex_positions, GL_STATIC_DRAW);
+    GLuint vbo = 0;
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertex_positions), vertex_positions, GL_STATIC_DRAW);
 
-  // Vertex attributes
-  // 0: vertex position (x, y, z)
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
-  glEnableVertexAttribArray(0);
+    // Vertex attributes
+    // 0: vertex position (x, y, z)
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+    glEnableVertexAttribArray(0);
 
-  // 1: vertex normals (x, y, z)
+    // 1: vertex normals (x, y, z)
 
-  // Unbind vbo (it was conveniently registered by VertexAttribPointer)
-  glBindBuffer(GL_ARRAY_BUFFER, 0);
+    // Unbind vbo (it was conveniently registered by VertexAttribPointer)
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-  // Unbind vao
-  glBindVertexArray(0);
+    // Unbind vao
+    glBindVertexArray(0);
 
-  // Uniforms
-  // - Model matrix
-  // - View matrix
-  // - Projection matrix
-  // - Normal matrix: normal vectors from local to world coordinates
-  // - Camera position
-  // - Light data
-  // - Material data
-  model_location = glGetUniformLocation(shader_program, "model");
-  view_location = glGetUniformLocation(shader_program, "view");
-  proj_location = glGetUniformLocation(shader_program, "projection");
-  // [...]
+    // Uniforms
+    // - Model matrix
+    // - View matrix
+    // - Projection matrix
+    // - Normal matrix: normal vectors from local to world coordinates
+    // - Camera position
+    // - Light data
+    // - Material data
+    model_location = glGetUniformLocation(shader_program, "model");
+    view_location = glGetUniformLocation(shader_program, "view");
+    proj_location = glGetUniformLocation(shader_program, "projection");
+    // [...]
 
 // Render loop
-  while(!glfwWindowShouldClose(window)) {
+    while(!glfwWindowShouldClose(window)) {
 
-    processInput(window);
+        processInput(window);
 
-    render(glfwGetTime());
+        render(glfwGetTime());
 
-    glfwSwapBuffers(window);
+        glfwSwapBuffers(window);
 
-    glfwPollEvents();
-  }
+        glfwPollEvents();
+    }
 
-  glfwTerminate();
+    glfwTerminate();
 
-  return 0;
+    return 0;
 }
 
 void render(double currentTime) {
-  float f = (float)currentTime * 0.3f;
+    float f = (float)currentTime * 0.3f;
 
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-  glViewport(0, 0, gl_width, gl_height);
+    glViewport(0, 0, gl_width, gl_height);
 
-  glUseProgram(shader_program);
-  glBindVertexArray(vao);
+    glUseProgram(shader_program);
+    glBindVertexArray(vao);
 
-  glm::mat4 model_matrix, view_matrix, proj_matrix;
+    glm::mat4 model_matrix, view_matrix, proj_matrix;
 
-  model_matrix = glm::mat4(1.f);
-  view_matrix = glm::lookAt(                 camera_pos,  // pos
-                            glm::vec3(0.0f, 0.0f, 0.0f),  // target
-                            glm::vec3(0.0f, 1.0f, 0.0f)); // up
+    model_matrix = glm::mat4(1.f);
+    view_matrix = glm::lookAt(                 camera_pos,  // pos
+                                               glm::vec3(0.0f, 0.0f, 0.0f),  // target
+                                               glm::vec3(0.0f, 1.0f, 0.0f)); // up
 
-  // Moving cube
-  // model_matrix = glm::rotate(model_matrix,
-  //   [...]
-  //
-  // Projection
-  // proj_matrix = glm::perspective(glm::radians(50.0f),
-  //   [...]
-  //
-  // Normal matrix: normal vectors to world coordinates
+    // Moving cube
+    // model_matrix = glm::rotate(model_matrix,
+    //   [...]
+    //
+    // Projection
+    // proj_matrix = glm::perspective(glm::radians(50.0f),
+    //   [...]
+    //
+    // Normal matrix: normal vectors to world coordinates
 
-  glDrawArrays(GL_TRIANGLES, 0, 36);
+    glDrawArrays(GL_TRIANGLES, 0, 36);
 }
 
 void processInput(GLFWwindow *window) {
-  if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-    glfwSetWindowShouldClose(window, 1);
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, 1);
 }
 
 // Callback function to track window size and update viewport
 void glfw_window_size_callback(GLFWwindow* window, int width, int height) {
-  gl_width = width;
-  gl_height = height;
-  printf("New viewport: (width: %d, height: %d)\n", width, height);
+    gl_width = width;
+    gl_height = height;
+    printf("New viewport: (width: %d, height: %d)\n", width, height);
 }
